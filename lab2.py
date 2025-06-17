c994177cb8d18e62259c1f0bc45158c40a49f3cd
Jennie Cruz Ramirez <jenniecruzramirez@Jennies-MacBook-Air.local>
2025-06-17 11:50:48 -0700

    added

---
+++ lab2.py
@@ -0,0 +1,16 @@
+# Lab 2: Python Profiles
+
+name = "Jennie Cruz Ramirez"
+
+nickname = 'Jen'
+
+age = 15
+
+used_python = False
+
+hobbies = ["playing videogames", "going out", "hanging out with friends"]
+
+favorite_things = {"game": "ROBLOX", "movie": "Sonic the hedgehog 3", "favotite thing to watch": "anime", "color": "navy blue"}
+
+print(used_python)
+
