6b7f26adf8e7f1a2bfdb58ca987b7e85c8661920
Jennie Cruz Ramirez <24jenniecruzramirez@gmail.com>
2025-06-24 15:34:50 -0700

    python profiles

--- lab2.py
+++ lab2.py
@@ -1,16 +1,34 @@
 # Lab 2: Python Profiles
 
-name = "Jennie Cruz Ramirez"
+extra = "Helloo!"
+name = "My name is Jennie Cruz Ramirez."
 
-nickname = 'Jen'
+# Nickname Variable
+nickname = 'Some people call me Jen, but I preferably like being called Jennie.'
 
+# Age Variable
 age = 15
 
+# Boolean Variable
 used_python = False
 
+# List Variable
 hobbies = ["playing videogames", "going out", "hanging out with friends"]
 
-favorite_things = {"game": "ROBLOX", "movie": "Sonic the hedgehog 3", "favotite thing to watch": "anime", "color": "navy blue"}
+# Dictionary Variable
+favorite_things = {"Fav Game": "ROBLOX", "Fav Movies": ["The Princess and the Frog", "Tangled", "The Notebook"], "Fav things to watch": ["Anime", "Movies", "Kdramas"], "Fav Color": "Navy blue", "Fav Show": "Young Sheldon"}
 
+# Set Variable
+teachers = {"Parker", "Cris", "Sydney", "Teju"}
+
+# Turple Variable
+teachers = ("Teju", "Sydney", "Cris", "Parker")
+
+print(extra)
+print(name)
+print(nickname)
+print(age)
 print(used_python)
-
+print(hobbies)
+print(favorite_things)
+print(teachers)
